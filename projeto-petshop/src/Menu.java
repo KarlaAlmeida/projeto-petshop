ebb0d8e0e6ad7207dada12242ca113a547f0048c
KarlaAlmeida <karlakcbam@gmail.com>
2025-03-24 12:22:49 -0300

    Add projeto em andamento

--- projeto-petshop/src/Menu.java
+++ projeto-petshop/src/Menu.java
@@ -25,9 +25,13 @@ public class Menu {
            option = scanner.nextInt();
 
            switch (option) {
-            
+            case 4 -> verificarAgua();
+            case 5 -> verificarShampoo();
+            case 6 -> maquinaLavar.checkTemPetNaMaquina();
             case 7 -> colocarPetNaMaquina();
-                
+            case 8 -> maquinaLavar.retirarPet();
+            case 9 -> maquinaLavar.limparMaquina();
+                            
            }
             
 
@@ -44,5 +48,20 @@ public class Menu {
         var pet = new Pet(name);
         maquinaLavar.setPet(pet);
         System.out.println("O pet " + pet.getName() + " foi colocado na máquina.");
+    }
+
+    public static void checkTemPetNaMaquina(){
+        var temPet = maquinaLavar.verificarPetNaMaquina();
+        System.out.println(temPet ? "Tem Pet na máquina." : "Não tem Pet na máquina.");
+    }
+
+    public static void verificarShampoo(){
+        var quantidade = maquinaLavar.getShampoo();
+        System.out.println("A máquina está com " + quantidade + " litro(s) de shampoo.");
+    }
+
+    public static void verificarAgua(){
+        var quantidade = maquinaLavar.getAgua();
+        System.out.println("A máquina está com " + quantidade + " litro(s) de água.");
     }
 }
